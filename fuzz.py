deb1cf77bacff46694b6a9bd9dfc2eb4e2a21533
Captain D <davex@vip.qq.com>
2015-04-09 12:11:16 +0800

    Add The Output & One Thread Version

--- fuzz.py
+++ fuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- utf8 -*-
 # author=dave
-# creat=20150408
+# create=20150408
 
 """
 * A Tool For Fuzzing Sub-domain.
@@ -17,12 +17,14 @@ import random
 import re
 import requests
 import os
+from common.output import *
 TOP_LEVEL = []
 THREADS_NUM = 10
 DNS_LIST = []
 
 
 def get_ban_ip(dns, domain):
+    # In Case it's Pan analytical
     ban_ip = 'time out'
     while ban_ip == 'time out':
         try:
@@ -32,9 +34,10 @@ def get_ban_ip(dns, domain):
     return ban_ip
 
 
-def find_ip_from_dns(dns_server, domain):
+def find_ip_from_dns(dns_server, sub_domain):
+    # Core Content
     host = ''
-    for i in domain.split('.'):
+    for i in sub_domain.split('.'):
         host += chr(len(i))+i
     index = os.urandom(2)
     data = '%s\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00%s\x00\x00\x01\x00\x01' % (index, host)
@@ -50,14 +53,17 @@ def find_ip_from_dns(dns_server, domain):
     return ip_list
 
 
-def get_ip(sub_domain, ban_ip):
-    global DNS_LIST
+def get_ip(sub_domain, ban_ip, category, domain):
+    # Use Method find_ip_from_dns to Fetch ip
+    global DNS_LIST, TOP_LEVEL
     dns_server = random.choice(DNS_LIST)
-    #
     try:
         ip = find_ip_from_dns(dns_server, sub_domain)
         if ip != ban_ip and ip != []:
             print "[+] <Found> %s" % sub_domain
+            output_add(sub_domain, ip, category, domain)
+            if category == "TOP-LEVEL":
+                TOP_LEVEL.append(sub_domain)
             # print "[+] <Found> %s %s" % (sub_domain, ip)
             # dns_server, sub_domain, ip
     except Exception, e:
@@ -68,12 +74,18 @@ def get_ip(sub_domain, ban_ip):
 def start_fuzz(domain):
     global TOP_LEVEL, DNS_LIST
     print "[*] Target: %s" % domain
+    output_init(domain)
     file_handle = open("./dict/dns.dict")
     DNS_LIST = file_handle.read().split('\n')
-    ban_ip = get_ban_ip(random.choice(DNS_LIST), domain)
+    # In Case it's Pan analytical
+    dns = random.choice(DNS_LIST)
+    ban_ip = get_ban_ip(dns, domain)
+    while not ban_ip:
+        ban_ip = get_ban_ip(dns, domain)
     fuzz_top_domain(domain, ban_ip)
-    # fuzz_second_domain()
+    fuzz_second_domain(domain, ban_ip)
     print "[*] Done!"
+    output_finished(domain)
 
 
 def fuzz_top_domain(domain, ban_ip):
@@ -82,11 +94,11 @@ def fuzz_top_domain(domain, ban_ip):
     content_dict = file_handle.read().split('\n')
     for i in xrange(len(content_dict)):
         sub_domain = content_dict[i] + '.' + domain
-        get_ip(sub_domain, ban_ip)
+        get_ip(sub_domain, ban_ip, "TOP-LEVEL", domain)
     print "[*] TOP-LEVEL DOMAIN FINISHED..."
 
 
-def fuzz_second_domain():
+def fuzz_second_domain(domain, ban_ip):
     global TOP_LEVEL
     print "[*] SECOND-LEVEL DOMAIN FUZZING..."
     file_handle = open("./dict/second-level.dict")
@@ -94,7 +106,7 @@ def fuzz_second_domain():
     for j in xrange(len(TOP_LEVEL)):
         for i in xrange(len(content_dict)):
             sub_domain = content_dict[i] + '.' + TOP_LEVEL[j]
-
+            get_ip(sub_domain, ban_ip, "SECOND-LEVEL", domain)
     print "[*] SECOND-LEVEL DOMAIN FINISHED..."
 
 
@@ -111,12 +123,12 @@ def domain_verify(doamin):
 if __name__ == "__main__":
     start_time = datetime.datetime.now()
     print "[*] FuzSub is hot."
-    if len(sys.argv) != 1:
+    if len(sys.argv) == 1:
         print "[-] Error! You should input the domain you want to Fuzz."
         print "[-] E.g. python fuzz.py foo.com"
     else:
-        # domain = sys.argv[1]
-        domain = "qq.com"
+        domain = sys.argv[1]
+        # domain = "qq.com"
         start_fuzz(domain)
     end_time = datetime.datetime.now()
     print '[*] Total Time Consumption: ' + str((end_time - start_time).seconds) + 's'
